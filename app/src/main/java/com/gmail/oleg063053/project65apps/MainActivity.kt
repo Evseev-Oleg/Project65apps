86dffaddbcfccd91aafa4b2a9d46354c56f7eb12
Evseev Oleg <oleg063053@gmail.com>
2021-10-10 19:17:06 +0400

    добавил проект с пустой активностью

---
+++ app/src/main/java/com/gmail/oleg063053/project65apps/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.oleg063053.project65apps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
